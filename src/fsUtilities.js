2d90afffa6eab85581f24e83b340559862dcfb95
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-12-15 16:20:15 +0100

    added the comment routes

--- src/fsUtilities.js
+++ src/fsUtilities.js
@@ -1,26 +1,86 @@
-const { readJSON, writeJSON } = require("fs-extra")
+const {
+	readJSON,
+	writeJSON,
+	pathExists,
+	ensureFile,
+	readJson,
+} = require("fs-extra")
 const { join } = require("path")
+const uniqId = require("uniqid")
 
 const booksPath = join(__dirname, "./services/books/books.json")
+const commentPath = join(__dirname, "./services/comments/")
 
-const readDB = async filePath => {
-  try {
-    const fileJson = await readJSON(filePath)
-    return fileJson
-  } catch (error) {
-    throw new Error(error)
-  }
+const readDB = async (filePath) => {
+	try {
+		const fileJson = await readJSON(filePath)
+		return fileJson
+	} catch (error) {
+		throw new Error(error)
+	}
 }
 
 const writeDB = async (filePath, fileContent) => {
-  try {
-    await writeJSON(filePath, fileContent)
-  } catch (error) {
-    throw new Error(error)
-  }
+	try {
+		await writeJSON(filePath, fileContent)
+	} catch (error) {
+		throw new Error(error)
+	}
+}
+
+const appendcomment = async (bookid, comment) => {
+	let path = join(commentPath, bookid + ".json")
+	ensureFile(path)
+	console.log("bookid ", bookid, " comment ", comment, " path ", path)
+	let comments = []
+	try {
+		comments = await readJSON(path)
+	} catch (error) {
+		console.log("comments now ", comments)
+		console.error(error)
+	}
+	console.log(comments)
+	comment.id = uniqId()
+	comments.push(comment)
+	console.log("comments to save ", comments)
+	try {
+		writeJSON(path, comments)
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+const getComments = async (bookId) => {
+	let path = join(commentPath, bookId + ".json")
+	try {
+		let result = await readJson(path)
+		return result
+	} catch (error) {
+		console.log("no such files")
+		return error
+	}
+}
+
+const deleteComments = async (bookId, commentId) => {
+	let path = join(commentPath, bookId + ".json")
+	let comments = []
+	console.log(comments)
+	try {
+		comments = await readJSON(path)
+		console.log("---")
+		comments = comments.filter((comment) => comment.id !== commentId)
+		writeJSON(path, comments)
+	} catch (error) {
+		console.log("not fount")
+		console.log(comments)
+		console.error(error)
+	}
 }
 
 module.exports = {
-  getBooks: async () => readDB(booksPath),
-  writeBooks: async booksData => writeDB(booksPath, booksData),
+	getBooks: async () => readDB(booksPath),
+	writeBooks: async (booksData) => writeDB(booksPath, booksData),
+	appendcomment,
+	getComments,
+	deleteComments,
 }
--- src/services/books/index.js
+++ src/services/books/index.js
@@ -1,127 +1,164 @@
 const express = require("express")
 
-const { getBooks, writeBooks } = require("../../fsUtilities")
+const {
+	getBooks,
+	writeBooks,
+	appendcomment,
+	getComments,
+	deleteComments,
+} = require("../../fsUtilities")
 
 const booksRouter = express.Router()
 
 booksRouter.get("/", async (req, res, next) => {
-  try {
-    const books = await getBooks()
+	try {
+		const books = await getBooks()
 
-    if (req.query && req.query.category) {
-      const filteredBooks = books.filter(
-        book =>
-          book.hasOwnProperty("category") &&
-          book.category === req.query.category
-      )
-      res.send(filteredBooks)
-    } else {
-      res.send(books)
-    }
-  } catch (error) {
-    console.log(error)
-    next(error)
-  }
+		if (req.query && req.query.category) {
+			const filteredBooks = books.filter(
+				(book) =>
+					book.hasOwnProperty("category") &&
+					book.category === req.query.category
+			)
+			res.send(filteredBooks)
+		} else {
+			res.send(books)
+		}
+	} catch (error) {
+		console.log(error)
+		next(error)
+	}
 })
 
 booksRouter.get("/:asin", async (req, res, next) => {
-  try {
-    const books = await getBooks()
+	try {
+		const books = await getBooks()
 
-    const bookFound = books.find(book => book.asin === req.params.asin)
+		const bookFound = books.find((book) => book.asin === req.params.asin)
 
-    if (bookFound) {
-      res.send(bookFound)
-    } else {
-      const err = new Error()
-      err.httpStatusCode = 404
-      next(err)
-    }
-  } catch (error) {
-    console.log(error)
-    next(error)
-  }
+		if (bookFound) {
+			res.send(bookFound)
+		} else {
+			const err = new Error()
+			err.httpStatusCode = 404
+			next(err)
+		}
+	} catch (error) {
+		console.log(error)
+		next(error)
+	}
 })
 
 booksRouter.post("/", async (req, res, next) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      const error = new Error()
-      error.message = errors
-      error.httpStatusCode = 400
-      next(error)
-    } else {
-      const books = await getBooks()
+	try {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			const error = new Error()
+			error.message = errors
+			error.httpStatusCode = 400
+			next(error)
+		} else {
+			const books = await getBooks()
 
-      const asinFound = books.find(book => book.asin === req.body.asin)
+			const asinFound = books.find((book) => book.asin === req.body.asin)
 
-      if (asinFound) {
-        const error = new Error()
-        error.httpStatusCode = 400
-        error.message = "Book already in db"
-        next(error)
-      } else {
-        books.push(req.body)
-        await writeBooks(books)
-        res.status(201).send({ asin: req.body.asin })
-      }
-    }
-  } catch (error) {
-    console.log(error)
-    const err = new Error("An error occurred while reading from the file")
-    next(err)
-  }
+			if (asinFound) {
+				const error = new Error()
+				error.httpStatusCode = 400
+				error.message = "Book already in db"
+				next(error)
+			} else {
+				books.push(req.body)
+				await writeBooks(books)
+				res.status(201).send({ asin: req.body.asin })
+			}
+		}
+	} catch (error) {
+		console.log(error)
+		const err = new Error("An error occurred while reading from the file")
+		next(err)
+	}
 })
 
 booksRouter.put("/:asin", async (req, res, next) => {
-  try {
-    const validatedData = matchedData(req)
-    const books = await getBooks()
+	try {
+		const validatedData = matchedData(req)
+		const books = await getBooks()
 
-    const bookIndex = books.findIndex(book => book.asin === req.params.asin)
+		const bookIndex = books.findIndex((book) => book.asin === req.params.asin)
 
-    if (bookIndex !== -1) {
-      // book found
-      const updatedBooks = [
-        ...books.slice(0, bookIndex),
-        { ...books[bookIndex], ...validatedData },
-        ...books.slice(bookIndex + 1),
-      ]
-      await writeBooks(updatedBooks)
-      res.send(updatedBooks)
-    } else {
-      const err = new Error()
-      err.httpStatusCode = 404
-      next(err)
-    }
-  } catch (error) {
-    console.log(error)
-    const err = new Error("An error occurred while reading from the file")
-    next(err)
-  }
+		if (bookIndex !== -1) {
+			// book found
+			const updatedBooks = [
+				...books.slice(0, bookIndex),
+				{ ...books[bookIndex], ...validatedData },
+				...books.slice(bookIndex + 1),
+			]
+			await writeBooks(updatedBooks)
+			res.send(updatedBooks)
+		} else {
+			const err = new Error()
+			err.httpStatusCode = 404
+			next(err)
+		}
+	} catch (error) {
+		console.log(error)
+		const err = new Error("An error occurred while reading from the file")
+		next(err)
+	}
 })
 
 booksRouter.delete("/:asin", async (req, res, next) => {
-  try {
-    const books = await getBooks()
+	try {
+		const books = await getBooks()
 
-    const bookFound = books.find(book => book.asin === req.params.asin)
+		const bookFound = books.find((book) => book.asin === req.params.asin)
 
-    if (bookFound) {
-      const filteredBooks = books.filter(book => book.asin !== req.params.asin)
+		if (bookFound) {
+			const filteredBooks = books.filter(
+				(book) => book.asin !== req.params.asin
+			)
 
-      await writeBooks(filteredBooks)
-      res.status(204).send()
-    } else {
-      const error = new Error()
-      error.httpStatusCode = 404
-      next(error)
-    }
-  } catch (error) {
-    console.log(error)
-    next(error)
-  }
+			await writeBooks(filteredBooks)
+			res.status(204).send()
+		} else {
+			const error = new Error()
+			error.httpStatusCode = 404
+			next(error)
+		}
+	} catch (error) {
+		console.log(error)
+		next(error)
+	}
+})
+
+booksRouter.post("/:asin/comments", async (req, res, next) => {
+	try {
+		await appendcomment(req.params.asin, req.body)
+		res.status(200).send("ok")
+	} catch (error) {
+		next(error)
+	}
+})
+
+booksRouter.get("/:asin/comments", async (req, res, next) => {
+	try {
+		let result = await getComments(req.params.asin)
+		res.status(200).send(result)
+	} catch (error) {
+		next(error)
+	}
+})
+
+booksRouter.delete("/:asin/comments/:id", async (req, res, next) => {
+	console.log(req.params.id)
+	try {
+		deleteComments(req.params.asin, req.params.id)
+		res.status(200).send("ok")
+	} catch (error) {
+		error.httpStatusCode(500)
+		next(error)
+	}
 })
 
 module.exports = booksRouter
